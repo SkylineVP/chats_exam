8dc0ac0a948412365d562ddd1d2ce878c6dbf0c4
vladpolizp@gmail.com <Skylinevp220>
2020-04-03 18:45:58 +0300

    Chat Controller create get and join chat

--- server/src/controllers/chat.controller.js
+++ server/src/controllers/chat.controller.js
@@ -3,7 +3,7 @@ const {Chat} = require('./../models');
 
 module.exports.createChat = async ( req, res, next ) => {
 	try {
-		console.log(req.headers.authorization);
+
 		const chat = await Chat.create({
 			...req.body,
 			owner: req.headers.authorization
@@ -27,4 +27,32 @@ module.exports.getChats = async ( req, res, next ) => {
 	} catch (e) {
 		next(e);
 	}
-}
+};
+module.exports.getChat = async ( req, res, next ) => {
+	try {
+		const chat = await Chat.findOne({_id: req.body.chatId});
+		if (chat) {
+			req.chat = chat;
+			next();
+		}
+		else {
+			res.status(404).send(new Error(`Chat:${req.chatId} Not Found`))
+		}
+	} catch (e) {
+		console.error(e)
+	}
+};
+module.exports.joinToChat = async ( req, res, next ) => {
+	try {
+		const {chat, headers: {authorization}, user} = req;
+		if (!chat.users.includes(authorization)) {
+			chat.users.push(authorization);
+			user.chats.push(chat._id);
+			chat.save();
+			user.save();
+			res.send('You joined');
+		}
+	} catch (e) {
+		console.error(e)
+	}
+};
