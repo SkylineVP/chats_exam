5e0527f31c477ade14b3807eb331378485c3d5b7
vladpolizp@gmail.com <Skylinevp220>
2020-04-09 14:12:18 +0300

    fix multer

--- server/src/middleware/multer.js
+++ server/src/middleware/multer.js
@@ -3,26 +3,26 @@ const fs = require('fs');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+	destination: function ( req, file, cb ) {
 
-    const fileDestination = path.resolve(__dirname,
-      `./../../uploads/profilePicture`);
+		const fileDestination = path.resolve(__dirname,
+			'..', '..', 'uploads', 'profilePicture');
 
-    if ( !fs.existsSync(fileDestination)) {
-      fs.mkdirSync(fileDestination);
-    }
+		if (!fs.existsSync(fileDestination)) {
+			fs.mkdirSync(fileDestination);
+		}
 
-    cb(null, fileDestination);
-  },
-  filename: function (req, file, cb) {
+		cb(null, fileDestination);
+	},
+	filename: function ( req, file, cb ) {
 
-    const extension = path.extname(file.originalname);
-    const newFileName = `${ Date.now() }${ extension }`;
-    cb(null, newFileName);
-  },
+		const extension = path.extname(file.originalname);
+		const newFileName = `${Date.now()}${extension}`;
+		cb(null, newFileName);
+	},
 });
 
 module.exports.saveProfilePicture = multer({
-  storage,
+	storage,
 }).single('profilePicture');
 
